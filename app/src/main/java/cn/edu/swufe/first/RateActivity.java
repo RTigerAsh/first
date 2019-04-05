0f930a6493340c16639902788aec3a7b1046ac5d
RTigerAsh <davidbamboozzz@outlook.com>
2019-04-05 20:25:01 +0800

    完成了汇率换算的基本程序
    额外的汇率修改部分做好了框架，有BUG

--- app/src/main/java/cn/edu/swufe/first/RateActivity.java
+++ app/src/main/java/cn/edu/swufe/first/RateActivity.java
@@ -2,6 +2,7 @@ package cn.edu.swufe.first;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,15 +11,19 @@ import android.widget.Toast;
 
 public class RateActivity extends AppCompatActivity {
 
-        EditText rmb;
+        EditText rmb,edit_dollar,edit_euro,edit_con;
         TextView showrate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rate);
 
         rmb=(EditText) findViewById(R.id.rmb);
+        edit_dollar=(EditText)findViewById(R.id.edit_dollar);
+        edit_euro=(EditText)findViewById(R.id.edit_euro);
+        edit_con=(EditText)findViewById(R.id.edit_con);
         showrate=(TextView)findViewById(R.id.showrate);
 
         Button dollar = findViewById(R.id.dollar);
@@ -32,33 +37,51 @@ public class RateActivity extends AppCompatActivity {
         euro.setOnClickListener(listener);
         con.setTag(3);
         con.setOnClickListener(listener);
+        edit_dollar.setTag(4);
+        edit_dollar.setOnClickListener(listener);
+        edit_euro.setTag(5);
+        edit_euro.setOnClickListener(listener);
+        edit_con.setTag(6);
+        edit_con.setOnClickListener(listener);
 
     }
 
     public class MyListener implements View.OnClickListener {
 
-        String str=rmb.getText().toString();
-
-        double r=Double.parseDouble(str);
-
         public void onClick(View v) {
+
             int tag =(Integer)v.getTag();
+
+            String str=rmb.getText().toString();
+            String strdo=edit_dollar.getText().toString();
+            String streu=edit_euro.getText().toString();
+            String strco=edit_con.getText().toString();
+
+            double dorate=1/6.7;
+            double eurate=1/11.0;
+            double corate=500;
+
             switch (tag){
+
                 case 1:
+
                     if (str.length()>0){
-                        double var=r;
-                        var=r*(1/6.7);
-                        String bs=String.format("%.2f",var);
+                        double r=Double.parseDouble(str);
+                        r=r*(1/dorate);
+                        String bs=String.format("%.2f",r);
                         showrate.setText(bs);
                     }else {
+                        //Log.i("main", "click finished"+str);
                         Toast.makeText(RateActivity.this,"请输入金额",Toast.LENGTH_SHORT).show();
                     }
                     break;
+
                 case 2:
+
                     if (str.length()>0){
-                        double var=r;
-                        var=r*(1/11d);
-                        String bs=String.format("%.2f",var);
+                        double r=Double.parseDouble(str);
+                        r=r*(1/eurate);
+                        String bs=String.format("%.2f",r);
                         showrate.setText(bs);
                     }else {
                         Toast.makeText(RateActivity.this,"请输入金额",Toast.LENGTH_SHORT).show();
@@ -66,21 +89,42 @@ public class RateActivity extends AppCompatActivity {
                     break;
 
                 case 3:
+
                     if (str.length()>0){
-                        double var=r;
-                        var=r*500;
-                        String bs=String.format("%.2f",var);
+                        double r=Double.parseDouble(str);
+                        r=r*corate;
+                        String bs=String.format("%.2f",r);
                         showrate.setText(bs);
                     }else {
                         Toast.makeText(RateActivity.this,"请输入金额",Toast.LENGTH_SHORT).show();
                     }
                     break;
+                case 4:
+                    if (strdo.length()>0){
+                        Double edit_dollar= Double.parseDouble(strdo);
+                        dorate=edit_dollar;
+                        Toast.makeText(RateActivity.this,"汇率已修改",Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(RateActivity.this,"汇率未修改",Toast.LENGTH_SHORT).show();
+                    }
+                case 5:
+                    if (streu.length()>0){
+                        Double edit_dollar= Double.parseDouble(strdo);
+                        dorate=edit_dollar;
+                        Toast.makeText(RateActivity.this,"汇率已修改",Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(RateActivity.this,"汇率未修改",Toast.LENGTH_SHORT).show();
+                    }
+                case 6:
+                    if (strco.length()>0){
+                        Double edit_dollar= Double.parseDouble(strdo);
+                        dorate=edit_dollar;
+                        Toast.makeText(RateActivity.this,"汇率已修改",Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(RateActivity.this,"汇率未修改",Toast.LENGTH_SHORT).show();
+                    }
+
             }
-
-
-
         }
-
-
     }
 }
